c3ab51f2e1117fb1456abdcd2a0880de9387cd3b
John Kalberer <john@Johns-Mac-mini.local>
2014-08-22 11:42:50 -0700

    Moving files and beginning work on protocols.

---
+++ Linkslap.iOS.Communication/Protocols/IStreamStore.swift
@@ -0,0 +1,13 @@
+//
+//  IStreamStore.swift
+//  Linkslap.iOS
+//
+//  Created by John Kalberer on 8/22/14.
+//  Copyright (c) 2014 Linkslap. All rights reserved.
+//
+
+import Foundation
+
+public protocol StreamStoreProtocol {
+    
+}
